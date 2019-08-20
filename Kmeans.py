fc03d7231544f1550d5456a5eab63b7040aa82e0
qc <qiancheng1948@outlook.com>
2019-08-20 21:27:18 +0800

    更新：Kmeans|distortion曲线选择K值

--- Kmeans.py
+++ Kmeans.py
@@ -4,7 +4,7 @@ issues:
     - Optimization objcet is minimizing the distortion/cost function (1/m)Sum||x-u||^2
     - Different initializations will get different cluster results (more seriously when k is small), sometimes gets local optimal
     - Repeat clustering for 50~1000 times to choose the lowest distortion
-    -
+    - Choose k value usually use Elbow method (curve : distortion/other business metric -- k value)
 
     - Initialize randomly from R^n instead of D will probably cause cluster disappearing (the cluster cannot get a single point)
     - Initialize from D cause cluster disappearing ?
@@ -14,13 +14,14 @@ issues:
 import numpy as np
 import pandas as pd
 import copy
+import matplotlib.pyplot as plt
 
 class kmeans(object):
 
-    def __init__(self, k):
-        self.k = k
+    def __init__(self):
+        pass
 
-    def fit(self, x, repeat = 1):
+    def fit(self, x, k = 3, repeat = 1):
         '''
         input x: a np.array/matrix or pd.DataFrame, each element must be number (int/float/...)
         output : a new column "cluster" appended to x
@@ -30,7 +31,7 @@ class kmeans(object):
 
             # randomly initialization: the cluster label is represented by center.index
             x_ = pd.DataFrame(x)
-            center = x_.sample(n=self.k, replace=False, axis=0).reset_index(drop=True)
+            center = x_.sample(n=k, replace=False, axis=0).reset_index(drop=True)
 
             # Iteration
             while 1:
@@ -61,4 +62,25 @@ class kmeans(object):
             if J < pre_J:
                 result = copy.deepcopy(x_)
 
-        return result # return a new DataFrame instead of modifying the original x
+        print('---- K = %d finished. ----'%k)
+
+        return result,J # return a new DataFrame instead of modifying the original x
+
+    def chooseK(self, x, repeat = 5, mink = 2, maxk = 10):
+        '''
+        Elbow method: draw the cruve (Distortion-K)
+        Better way is to evaluating by (A business metric-K)
+
+        return a curve : distortion -- k
+        and the distortion vector
+        '''
+        Distortion = []
+        for k in range(mink, maxk+1):
+            Distortion.append(self.fit(x,repeat=repeat,k=k)[1])
+
+        plt.plot(range(mink,maxk+1),Distortion)
+        plt.xlabel('Cluster')
+        plt.ylabel('Distortion')
+        plt.show()
+
+        return Distortion
